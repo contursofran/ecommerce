792f52d7bfb18ed641d84afea96cd44d16e967ff
Franco Conturso <contursofran@gmail.com>
2024-07-20 21:43:45 -0300

    feat: add topbar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-col">
+        <div className="bg-[#0A0909] flex flex-col">
           <Topbar />
           {children}
         </div>
--- frontend/components/topbar.tsx
+++ frontend/components/topbar.tsx
@@ -3,23 +3,36 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+interface NavLinkProps {
+  href: string;
+  currentPath: string;
+  children: React.ReactNode;
+}
+
+const NavLink = ({ href, currentPath, children }: NavLinkProps) => {
+  const isActive = currentPath === href;
+  return (
+    <Link href={href} className="h-full items-center flex">
+      <div className={isActive ? "font-bold" : "text-[#fffc]"}>{children}</div>
+    </Link>
+  );
+};
+
 export default function Topbar() {
   const currentPath = usePathname();
 
   return (
-    <nav className="h-12 max-w-5xl">
-      <div className="flex gap-12 font-medium text-base">
-        <Link href="/">
-          <div className={currentPath === "/" ? "font-bold" : ""}>Home</div>
-        </Link>
-        <Link href="/shop">
-          <div className={currentPath === "/shop" ? "font-bold" : ""}>Shop</div>
-        </Link>
-        <Link href="/about">
-          <div className={currentPath === "/about" ? "font-bold" : ""}>
-            About
-          </div>
-        </Link>
+    <nav className="h-12 max-w-5xl px-6 border border-[#1a1b1b] bg-black rounded-2xl mx-auto mt-5 w-full flex items-center">
+      <div className="flex gap-12 font-medium h-full items-center text-sm">
+        <NavLink href="/" currentPath={currentPath}>
+          Home
+        </NavLink>
+        <NavLink href="/shop" currentPath={currentPath}>
+          Shop
+        </NavLink>
+        <NavLink href="/about" currentPath={currentPath}>
+          About
+        </NavLink>
       </div>
     </nav>
   );
