ec250f8bfcf81cd0e52d083a27be666f5d50394d
Franco Conturso <contursofran@gmail.com>
2024-07-22 16:44:33 -0300

    chore: add shadcn

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      asd
+      <h1 className="text-4xl font-bold mt-20">
+        E-Commerce built with React and .NET
+      </h1>
+      <p className="text-lg mt-5">
+        This is a personal project made to learn about React and .NET
+      </p>
     </main>
   );
 }
