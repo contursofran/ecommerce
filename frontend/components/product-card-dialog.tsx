4cb469b22ae01bb1cf58aa95913bc41370e22a1f
Franco Conturso <contursofran@gmail.com>
2024-07-28 22:50:52 -0300

    feat: add drawer for products

---
+++ frontend/components/product-card-dialog.tsx
@@ -0,0 +1,74 @@
+import {
+  DialogContent,
+  DialogTrigger,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Dialog } from "@radix-ui/react-dialog";
+import Image from "next/image";
+import { Price } from "@/components/Price";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
+interface ProductCardPopoverProps {
+  title: string;
+  price: number;
+  image: string;
+  description?: string;
+  children: React.ReactNode;
+}
+
+export function ProductCardDialog({
+  children,
+  image,
+  title,
+  price,
+}: ProductCardPopoverProps) {
+  return (
+    <Dialog>
+      <DialogTrigger>{children}</DialogTrigger>
+      <DialogContent className="w-[800px]">
+        <div className="h-[450px] flex justify-between ">
+          <div className="relative flex items-center justify-center w-1/2">
+            <Image fill src={image} className="object-contain" alt={title} />
+          </div>
+          <div className="flex pl-4 pt-4 flex-col h-full w-1/2">
+            <div className="flex flex-col h-full w-full gap-3">
+              <h1 className="text-lg font-semibold">{title}</h1>
+              <Price
+                className="flex-none rounded-full w-fit text-xs bg-white font-semibold p-2 text-black"
+                amount={price.toString()}
+                currencyCodeClassName="hidden @[275px]/label:inline"
+                currencyCode="USD"
+              />
+              <div className="h-[0.8px] w-full bg-[#2e2e2e]"></div>
+              <div className="font-semibold mt-4 text-xs flex flex-col gap-3">
+                <h2>COLOR</h2>
+                <div className="flex line-through gap-4">
+                  <Badge variant="outline">Red</Badge>
+                  <Badge variant="outline">White</Badge>
+                  <Badge variant="outline">Black</Badge>
+                </div>
+                <h2 className="mt-6">SIZE</h2>
+                <div className="flex gap-4 line-through">
+                  <Badge variant="outline">Small</Badge>
+                  <Badge variant="outline">Medium</Badge>
+                  <Badge variant="outline">Large</Badge>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-end pb-2 pr-4">
+              <Button className="w-fit flex">Add to cart</Button>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- frontend/components/product-card.tsx
+++ frontend/components/product-card.tsx
@@ -1,7 +1,9 @@
+import { ProductCardDialog } from "@/components/product-card-dialog";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Plus, ShoppingBagIcon } from "lucide-react";
 import Image from "next/image";
+import { Price } from "./Price";
 interface ProductCardProps {
   title: string;
   price: number;
@@ -10,59 +12,38 @@ interface ProductCardProps {
 
 export function ProductCard({ title, price, image }: ProductCardProps) {
   return (
-    <div className="bg-black rounded-2xl shadow-lg hover:border-2 hover:border-white hover:cursor-pointer  text-white w-full border border-[#2E2E2E] col-span-1 row-span-1 h-full aspect-square">
-      <div className="relative h-full w-full">
-        <Image
-          fill
-          src={image}
-          alt={title}
-          className="
-           object-contain w-full h-full rounded-2xl"
-        />
-        <div
-          className={cn(
-            "absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label",
-            {
-              "lg:px-20 lg:pb-[35%]": false,
-            }
-          )}
-        >
-          <div className="flex border-[#2E2E2E] bg-transparent/80 items-center rounded-full border p-1 text-xs font-semibold text-white ">
-            <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">
-              {title}
-            </h3>
-            <Price
-              className="flex-none rounded-full bg-white p-2 text-black"
-              amount={price.toString()}
-              currencyCodeClassName="hidden @[275px]/label:inline"
-              currencyCode="USD"
-            />
+    <ProductCardDialog title={title} price={price} image={image}>
+      <div className="bg-black rounded-2xl shadow-lg hover:border-2 hover:border-white hover:cursor-pointer  text-white w-full border border-[#2E2E2E] col-span-1 row-span-1 h-full aspect-square">
+        <div className="relative h-full w-full">
+          <Image
+            fill
+            src={image}
+            alt={title}
+            className="
+          object-contain w-full h-full rounded-2xl"
+          />
+          <div
+            className={cn(
+              "absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label",
+              {
+                "lg:px-20 lg:pb-[35%]": false,
+              }
+            )}
+          >
+            <div className="flex border-[#2E2E2E] bg-transparent/80 items-center rounded-full border p-1 text-xs font-semibold text-white ">
+              <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">
+                {title}
+              </h3>
+              <Price
+                className="flex-none rounded-full bg-white p-2 text-black"
+                amount={price.toString()}
+                currencyCodeClassName="hidden @[275px]/label:inline"
+                currencyCode="USD"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ProductCardDialog>
   );
 }
-
-const Price = ({
-  amount,
-  className,
-  currencyCode = "USD",
-  currencyCodeClassName,
-}: {
-  amount: string;
-  className?: string;
-  currencyCode: string;
-  currencyCodeClassName?: string;
-} & React.ComponentProps<"p">) => (
-  <p suppressHydrationWarning={true} className={className}>
-    {`${new Intl.NumberFormat(undefined, {
-      style: "currency",
-      currency: currencyCode,
-      currencyDisplay: "narrowSymbol",
-    }).format(parseFloat(amount))}`}
-    <span
-      className={cn("ml-1 inline", currencyCodeClassName)}
-    >{`${currencyCode}`}</span>
-  </p>
-);
