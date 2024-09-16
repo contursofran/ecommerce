f7b3324cdc480d6cb008775094df113705f9817c
Franco Conturso <contursofran@gmail.com>
2024-09-15 21:57:04 -0300

    feat: add Product model and DataContext for EF Core integration; update project references and ignore development appsettings

---
+++ backend/Domain/Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Domain;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string Category { get; set; }
+}
