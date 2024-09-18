4ea9218f6852b576d612ab03c658667c942b7c93
Franco Conturso <contursofran@gmail.com>
2024-09-17 23:30:29 -0300

    refactor: update data fetching method in ShopPage component

--- frontend/app/shop/page.tsx
+++ frontend/app/shop/page.tsx
@@ -1,15 +1,17 @@
 import { ProductCard } from "@/components/product-card";
 import { Product } from "@/types";
 import { promises as fs } from "fs";
-async function getDataFromJsonFile() {
-  const file = await fs.readFile(process.cwd() + "/data.json", "utf8");
-  const data = JSON.parse(file);
-  return data;
+
+async function getData() {
+  const res = await fetch("http://localhost:5247/api/Products");
+  if (res.ok) {
+    return res.json();
+  }
 }
 
 export default async function ShopPage() {
-  const data = await getDataFromJsonFile();
-
+  const data = await getData();
+  console.log(data);
   return (
     <div className="pt-8 max-w-5xl grid grid-cols-3 gap-8 mx-auto w-full">
       {data.map((product: Product) => (
