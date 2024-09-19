4470fced87390b6fb2b6dbbf4288aebbfe443b4e
Franco Conturso <contursofran@gmail.com>
2024-09-18 22:44:40 -0300

    refactor: improve data fetching method in ShopPage component with enhanced logging and error handling

--- frontend/app/shop/page.tsx
+++ frontend/app/shop/page.tsx
@@ -1,23 +1,28 @@
 import { ProductCard } from "@/components/product-card";
 import { Product } from "@/types";
-import { promises as fs } from "fs";
 
 async function getData() {
+  const apiUrl = process.env.API_URL + "/products";
+  console.log("Fetching products from:", apiUrl);
+
   try {
-    const res = await fetch(process.env.API_URL + "/products");
+    const res = await fetch(apiUrl);
     if (res.ok) {
       return res.json();
     } else {
       console.error("Failed to fetch products:", res.statusText);
+      return null;
     }
   } catch (error) {
     console.error("Network error:", error);
+    return null;
   }
 }
 
 export default async function ShopPage() {
   const data = await getData();
-  console.log(data);
+  console.log("Fetched data:", data);
+
   return (
     <div className="pt-8 max-w-5xl grid grid-cols-3 gap-8 mx-auto w-full">
       {data ? (
